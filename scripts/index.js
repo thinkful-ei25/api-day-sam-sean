4ad96f81cb25d334080144f14f8ae7f75dafa173
Sean Nealon <signalflowsean@gmail.com>
2018-10-03 14:03:02 -0400

    simplify the store update methods

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,21 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   api.getItems((items)=> {
-    console.log('hello there');
+    
+    //console.log('hello there');
     items.forEach((item) => store.addItem(item));
     shoppingList.render();
+
+    const item = store.items[0];
+    console.log('current name: ' + item.name);
+    store.findAndUpdate(item.id, { name: 'foobar' });
+    console.log('new name: ' + item.name);
+
+
+ 
   });
 });
+
 
 
 
